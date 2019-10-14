0001a969c922781ee192703dfba3f9f214f8f212
StanleyPalacios <54544780+StanleyPalacios@users.noreply.github.com>
2019-10-14 19:19:42 -0400

    LAB. 7

---
+++ ejerccio 1 labo 7.py
@@ -0,0 +1,8 @@
+def sumadenumeroImpares(n):
+    if(n==1):
+        return 1
+    else
+    return n + sumadenumrosImpares
+
+n=int(input("ingrese la cantidad de numeros impares a sumar")
+      print("la suma de los primeros numeros impares es de:")+str(ssumadenumeroImpares((n*2)-2) ))
